d0472a143092dd8609970b99d53a54f050fd7451
Bruce Wayne <bruce@wayne.com>
2026-03-05 05:57:17 +0530

    second commit

--- src/Gitdemopack/Demo.java
+++ src/Gitdemopack/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello hutey");
+		System.out.println("i am practicing for pull");
 	}
 
 }
